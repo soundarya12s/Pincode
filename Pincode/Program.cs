bf4e8f7770ffb90d3253976bf97f6efc53e347f5
S Soundarya <soundarya.s@tvsnext.com>
2023-08-02 21:45:19 +0530

    [Soundarya]Add:added in validate email

--- Pincode/Program.cs
+++ Pincode/Program.cs
@@ -6,11 +6,33 @@ namespace Pincode
     {
         static void Main(string[] args)
         {
-            ValidatePincode validatePincode = new ValidatePincode();
-            validatePincode.UC1_Validate("400088");
-            validatePincode.UC2_Validate("A400088");
-            validatePincode.UC3_Validate("400088A");
-            validatePincode.UC4_Validate("400 088");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter the option to execute\n 1.Validate Pincode\n 2.Validate Email\n 3.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        ValidatePincode validatePincode = new ValidatePincode();
+                        validatePincode.UC1_Validate("400088");
+                        validatePincode.UC2_Validate("A400088");
+                        validatePincode.UC3_Validate("400088A");
+                        validatePincode.UC4_Validate("400 088");
+                        break;
+                    case 2:
+                        ValidateEmail validateEmail = new ValidateEmail();
+                        validateEmail.UC1_Validate("abc");
+                        validateEmail.UC2_Validate("@bridgelabz");
+                        validateEmail.UC3_Validate(".co");
+                        validateEmail.UC4_Validate("abc.xyz");
+                        validateEmail.UC5_Validate("abc.xyz@bridgelabz.co.in");
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
---
+++ Pincode/ValidateEmail.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Pincode
+{
+    public class ValidateEmail
+    {
+        string UC1_Regex = "^[a-z]{0,}$";
+        string UC2_Regex = "^[@]{1}[a-z]{0,}$";
+        string UC3_Regex = "^[.]{1}[a-z]{2}$";
+        string UC4_Regex_Optional = "^[a-z]+[._+-]{0,1}[a-z]{0,}$";
+        string actualRegex = "^[a-z]+[._+-]{0,1}[a-z]+[@]{1}[a-z]+[.]{1}[a-z]{2,3}([.]{1}[a-z]{2}){0,1}$";
+
+        public void UC1_Validate(string input)
+        {
+            bool result = Regex.IsMatch(input, UC1_Regex);
+            if (result)
+                Console.WriteLine("Validation Successful");
+            else
+                Console.WriteLine("Validation Unsuccessful");
+        }
+        public void UC2_Validate(string input)
+        {
+            bool result = Regex.IsMatch(input, UC2_Regex);
+            if (result)
+                Console.WriteLine("Validation Successful");
+            else
+                Console.WriteLine("Validation Unsuccessful");
+        }
+        public void UC3_Validate(string input)
+        {
+            bool result = Regex.IsMatch(input, UC3_Regex);
+            if (result)
+                Console.WriteLine("Validation Successful");
+            else
+                Console.WriteLine("Validation Unsuccessful");
+        }
+        public void UC4_Validate(string input)
+        {
+            bool result = Regex.IsMatch(input, UC4_Regex_Optional);
+            if (result)
+                Console.WriteLine("Validation Successful");
+            else
+                Console.WriteLine("Validation Unsuccessful");
+        }
+        public void UC5_Validate(string input)
+        {
+            bool result = Regex.IsMatch(input, actualRegex);
+            if (result)
+                Console.WriteLine("Validation Successful");
+            else
+                Console.WriteLine("Validation Unsuccessful");
+        }
+    }
+}
