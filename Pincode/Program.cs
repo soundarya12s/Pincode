390665af2025f3deeb39c5586fc69c42bb7bd699
S Soundarya <soundarya.s@tvsnext.com>
2023-08-02 21:27:11 +0530

    [Soundarya]Added validate pincode feature

---
+++ Pincode/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Pincode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidatePincode validatePincode = new ValidatePincode();
+            validatePincode.Validate("400088");
+        }
+    }
+}
---
+++ Pincode/ValidatePincode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Pincode
+{
+    public class ValidatePincode
+    {
+        string uc1_Regex = "^[0-9]{6}$";
+        public void Validate(string input)
+        {
+            bool result = Regex.IsMatch(input, uc1_Regex);
+            if (result)
+                Console.WriteLine("Validation Successful");
+            else
+                Console.WriteLine("Invalidation Unsuccessful");
+        }
+    }
+}
